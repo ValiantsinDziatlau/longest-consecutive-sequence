3239dfbc529ee512aae147f505b43fbce7f99331
ValiantsinDziatlau <valiantsindziatlau@gmail.com>
2018-03-09 00:43:27 +0300

    longest-Consecutive-Length/feature: map function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+    let count=1;
+    let max=1;
+    function NoRepeat(currentValue, index, arr){
+       let a= arr.indexOf(currentValue);
+       let b = arr.lastIndexOf(currentValue);
+        array.splice(a+1, b-a);
+        if(currentValue+1===arr[index+1]){
+            count++;
+        }else{
+            if(count>max){
+                max=count;
+            }
+            count=1;
+        }
+    }
+    
+    if(array.length===0){
+        return 0;
+    }
+    array.sort((a, b)=>a-b);
+    //console.log(array);
+    array.map(NoRepeat);
+    return max;
 }
