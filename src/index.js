d0af3a1da0fb4f3885fb37119bca4f2a36fa0872
ValiantsinDziatlau <valiantsindziatlau@gmail.com>
2018-03-09 17:42:11 +0300

    longest-consecutive-sequence/feature: object for filter and sort array is used BigO(2n)

--- src/index.js
+++ src/index.js
@@ -1,25 +1,24 @@
 module.exports = function longestConsecutiveLength(array) {
-    let count=1;
-    let max=1;
-    function NoRepeat(currentValue, index, arr){
-       let a= arr.indexOf(currentValue);
-       let b = arr.lastIndexOf(currentValue);
-        array.splice(a+1, b-a);
-        if(currentValue+1===arr[index+1]){
-            count++;
-        }else{
-            if(count>max){
-                max=count;
-            }
-            count=1;
-        }
-    }
-    
-    if(array.length===0){
+    let obj={}, count=1, max=1;
+    let length=array.length;
+    if(!length){
         return 0;
     }
-    array.sort((a, b)=>a-b);
-    //console.log(array);
-    array.map(NoRepeat);
+    for(let i=0; i<length; i++){		
+		if(!obj[array[i]]){      
+      obj[array[i]] = true;
+   }        
+    }
+    for(var index in obj){
+        if(obj[index-0+1]){
+            count++;
+        } else{
+            if(count>max){
+                max=count;                
+            }
+			count=1;
+        }
+    }    
     return max;
+    
 }
